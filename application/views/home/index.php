f937804e1ce4c1a7e901e17953ff85a6eb603998
ckandrinirina <ckandrinirina@gmail.com>
2020-02-21 16:11:41 +0300

    dynamic data

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,8 @@
-<h3 class="text-center text-danger">SCRIPT</h3>
-<h5 class="text-center"><?= $title[0]['champs_libelle'] ?></h5>
-<div id="content"></div>
-<button id="previous">PREV</button> 
-<button id="next">NEXT</button>
-<script src="<?= base_url('assets/home/script.js') ?>"></script>
+<div class="container">
+    <h3 class="text-center text-danger">SCRIPT</h3>
+    <h5 class="text-center"><?= $title[0]['champs_libelle'] ?></h5>
+    <!-- dynamic content -->
+    <div id="content"></div>
+    <!-- dynamic content -->
+    <script src="<?= base_url('assets/home/script.js') ?>"></script>
+</div>
--- public/assets/home/script.js
+++ public/assets/home/script.js
@@ -1,20 +1,76 @@
+SCRIPT_VAL = [];
 $(document).ready(function () {
-    var actualStep = 2;
-    initByStep(actualStep);
+    //save actual data for best control
+    var dataActualStep = initByStep(1);
+
+    console.log(dataActualStep);
+    //click on next step
+    $('#content').on('click', '.radio', (e, v) => {
+        var $e = $(e.target);
+        //id of clicked element
+        var clickId = $e.prop('id');
+        //find data of clicked element
+        var dataActualClick = findResultInActualData(dataActualStep, clickId);
+        pushToScript(dataActualClick);
+        console.log(SCRIPT_VAL);
+        // goToNextStep();
+    })
 });
 
 //used to get content from the back-end
-function initByStep(step = 2) {
+function initByStep(step) {
     var url = base_url + 'home/ajaxGetContentByStep';
+    var dataStep;
     $.ajax({
         type: "GET",
         url: url,
+        async: false,
         data: {
             step: step
         },
         success: function (response) {
             //append view to content html
-            $('#content').append(response);
+            $('#content').append(response.html);
+            dataStep = response.data;
         }
     });
+    return dataStep;
 }
+//used to find result of clicked radio
+function findResultInActualData(dataActual, id) {
+    var thisData = [];
+    dataActual.forEach((element, index) => {
+        if (element.champs_id == id)
+            thisData = element;
+    });
+    return thisData;
+}
+
+//used to update or push data to sript
+function pushToScript(data) {
+    var testIfExist = false;
+    var idexIfExist;
+    //find if step is already saved in script
+    SCRIPT_VAL.forEach((element, index) => {
+        if (data.champs_num_step == element.script_num_step) {
+            testIfExist = true;
+            idexIfExist = index;
+        }
+    });
+    //update
+    if (testIfExist)
+        SCRIPT_VAL[idexIfExist] = {
+            script_num_step: data.champs_num_step,
+            script_step_fk: data.champs_id,
+        };
+    //insert
+    else
+        SCRIPT_VAL.push({
+            script_num_step: data.champs_num_step,
+            script_step_fk: data.champs_id,
+        });
+}
+
+// function goToNextStep(nextStep){
+//     dataActualStep = initByStep(nextStep);
+// }
