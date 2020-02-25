1059bef78d3468b7b633fdc1de401a624928dacf
evercollier <evercollier@creacticom.fr>
2020-02-25 17:35:29 +0100

    modification du design 25 février

--- application/views/default/navbar.php
+++ application/views/default/navbar.php
@@ -1,13 +1,23 @@
 <!-- Just an image -->
+<!-- nav with link to nextcloud -->
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">
     <img src="<?= base_url('assets/img/logoelise.png') ?>" alt="">
   </a>
-  <a class="navbar-brand" href="#">
-    <img src="<?= base_url('assets/img/chat.svg') ?>" alt="">
+  <div class="link-nextcloud">
+    <a href="http://46.105.120.43:8080/apps/mail/" target="_blank">
+    <img src="<?= base_url('assets/img/mail.png') ?>" alt="">
   </a>
+  <a href="http://46.105.120.43:8080/apps/spreed/" target="_blank">
+    <img src="<?= base_url('assets/img/chat.png') ?>" alt="">
+  </a>
+  </div>
 </nav>
+    <!-- nav with all the client links -->
 <div class="listeclient">
+  <div>
+    <a href="#"><p>Voir tous les clients</p></a>
+  </div>
   <a class="listeclient-item" href="#">
     <img src="<?= base_url('assets/img/actiglass.png') ?>" class="actiglass" alt="">
   </a>
