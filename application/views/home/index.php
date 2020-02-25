3c926d231d3064db2af32e2efa87f9371ef33732
ckandrinirina <ckandrinirina@gmail.com>
2020-02-25 12:03:41 +0300

    search

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h3 class="text-center text-danger">SCRIPT</h3>
-    <h5 class="text-center">Service d'accueil Bubendor, Elise Bonjour.</h5>
+    <h5 class="text-center">Service d'accueil Bubendorff, Elise Bonjour.</h5>
     <!-- dynamic content -->
     <div id="content" class="row"></div>
     <div class="contact"></div>
