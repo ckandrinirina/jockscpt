c486d07f58e66560e12c47c55db0af6ee09b1bc5
evercollier <evercollier@creacticom.fr>
2020-02-26 14:15:15 +0100

    modif design nav et bouton

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="btn-search">
-      <a href="https://www.bubendorff.com/trouver-revendeur-installateur/ " class="btn btn-default">Cherche un installateur</a>
-      <a href="https://www.bubendorff.com/demander-une-intervention/#/reparation" class="btn btn-default">Demande de réparation</a>
+      <a href="https://www.bubendorff.com/trouver-revendeur-installateur/ " target="_blank" class="btn btn-default">Chercher un installateur</a>
+      <a href="https://www.bubendorff.com/demander-une-intervention/#/reparation" target="_blank" class="btn btn-default">Demande de réparation</a>
     </div>
     <h3 class="text-center text-danger">SCRIPT</h3>
     <h5 class="text-center">Service d'accueil Bubendorff, Elise Bonjour.</h5>
